3b81cbf92f4865e6257acd9c26dc1ebfe3db8aaf
TanishqRawat0112 <21U02036@iiitbhopal.ac.in>
2024-05-31 02:13:36 +0530

    finished the ai summarizer with local storage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/TanishqRawat0112/summarizer", "_blank")
           }
           className='black_btn'
         >
@@ -20,9 +20,13 @@ const Hero = () => {
       </nav>
 
       <h1 className='head_text'>
-        Summarize Articles with <br />
+        Summarize Articles with <br className='max-md:hidden' />
         <span className='orange_gradient'>Open AI GPT-4</span>
       </h1>
+
+      <h2 className='desc'>
+          Simplify your reading with Sumz, an open source article summarizer  that transforms lengthy articles into clear and concise summaries.
+      </h2>
     </header>
   )
 }
